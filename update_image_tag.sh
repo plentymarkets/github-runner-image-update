843248cb0dfe2d5b73a6d8fd912b8eff1752a643
Madalin <madalin.bivolan@plentymarkets.com>
2022-12-06 11:31:28 +0200

    Getting username and user email from GitHub

--- update_image_tag.sh
+++ update_image_tag.sh
@@ -17,11 +17,41 @@ function clone_repo {
     fi
 }
 
+function get_user_details {
+    local skip_github="true"
+
+    if [[ "$GH_TOKEN" == "" ]]; then
+        echo "Warning: Could not find GH_TOKEN, using default user name and email"
+        return
+    fi
+
+    gh auth status && skip_github="false"
+    if [[ "$skip_github" == "true" ]]; then
+        echo "Warning: Could not login in GitHub, using default user name and email"
+        return
+    fi
+
+    gh api /user | jq '.login'
+    gh_user="$(gh api /user | jq -r '.login')"
+    if [[ "$gh_user" != "" ]]; then
+        echo "Using GitHub user: ${gh_user}"
+        export GIT_USER="${gh_user}"
+    fi
+
+    gh_mail="$(gh api /user/emails | jq -r '.[0].email')"
+    if [[ "$gh_mail" != "" ]]; then
+        echo "Using GitHub mail: ${gh_mail}"
+        export GIT_MAIL="${gh_mail}"
+    fi
+}
+
 REPO_URL="$1"
 RELEASE_TAG="$2"
 APP="$3"
 APP_VERSION="$4"
 CLUSTERS_REL_PAT="$5"
+
+get_user_details
 
 clone_repo "$REPO_URL" "./manifests"
 
